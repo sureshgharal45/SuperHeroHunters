564703c569b236e5600c49642f67c2e6f744185c
sureshgharal45 <sureshgharal65@gmail.com>
2023-06-30 17:49:17 +0530

    first commit

---
+++ Details/details.js
@@ -0,0 +1,51 @@
+let heroId = JSON.parse(localStorage.getItem("heroId"));
+
+let heroDetails = null;
+let titleEle = document.getElementById("name");
+let imgEle = document.getElementsByTagName("img")[0];
+let description = document.getElementById("description");
+let comics = document.getElementById("comics");
+let series = document.getElementById("series");
+let releaseDate = document.getElementById("release-data");
+let seriesNumber = document.getElementById("seriesNumber");
+let comicNumber = document.getElementById("comicNumber");
+async function fetchHeroDetails(id) {
+  const response = await fetch(
+    `https://gateway.marvel.com/v1/public/characters/${id}?apikey=c2595c6e10b8e75e6bd3b3c61b14547c&hash=77964d9b5c2bef6213992685d7c2dfd4&ts=1`
+  );
+  heroDetails = (await response.json()).data.results[0];
+  console.log(heroDetails);
+  if (heroDetails.description.length != 0)
+    description.innerHTML += `<h3>${heroDetails.description}</h3>`;
+  else description.innerHTML += `<h3>Description Not available</h3>`;
+  titleEle.innerHTML += `<h3> ${heroDetails.name} </h3>`;
+  let date = `${heroDetails.modified}`;
+  let parsedDate = "";
+  for (i of date) {
+    if (("0" <= i && i <= "9") || i == "-") parsedDate += i;
+    else break;
+  }
+  console.log("date",parsedDate);
+  releaseDate.innerHTML += `<h3>${parsedDate}</h3>`;
+  imgEle.setAttribute(
+    "src",
+    `${heroDetails.thumbnail.path}.${heroDetails.thumbnail.extension}`
+  );
+  seriesNumber.innerHTML += `${heroDetails.series.available}`;
+  let seriesNum = 1;
+  for (i of heroDetails.series.items) {
+    series.innerHTML += `<h3>Series Number ${seriesNum} : ${i.name}</h3>`;
+    seriesNum++;
+  }
+  comicNumber.innerHTML += `${heroDetails.comics.available}`;
+  let comicsNum = 1;
+  for (i of heroDetails.comics.items) {
+    comics.innerHTML += `<h3>Comic Number ${comicsNum}: ${i.name}</h3>`;
+    comicsNum++;
+  }
+}
+
+function heroLoad() {
+  fetchHeroDetails(heroId);
+}
+heroLoad();
---
+++ script.js
@@ -0,0 +1,109 @@
+//my apikey = 4a0de140540f73b7ebc8b9816b51defd
+//hash value = ef2adfa67e8de20cc93d4d4c9be0b04f
+
+let listOfHeroInDom = document.getElementById("heroList");
+let errorMessage = document.getElementById("errorMessage");
+let inputBar = document.getElementById("inputBar");
+
+let listOfHeros = [];
+let favList = [];
+function addHerosToDOM(hero) {
+  let li = document.createElement("li");
+
+  li.innerHTML = `
+    <img src="${
+      hero.thumbnail.path + "." + hero.thumbnail.extension
+    }" id = "poster"  >
+    <h2 id="heroTitle"  data-id=${hero.id}>${hero.name}</h2>
+    <button id="details" data-id="${hero.id}"> details </button>
+    <button id="favBtn" data-id="${hero.id}" data-title="${
+    hero.name
+  }" >Add to Favourites</button>
+    `;
+  listOfHeroInDom.append(li);
+}
+
+//to render the list
+function renderHeroList() {
+  listOfHeroInDom.innerHTML = "";
+  if (listOfHeros.length === 0) {
+    errorMessage.innerHTML = `No superhero found with that name`;
+    return;
+  }
+  listOfHeroInDom.innerHTML = "";
+  errorMessage.innerHTML = "";
+  for (let i = 0; i < listOfHeros.length; i++) {
+    addHerosToDOM(listOfHeros[i]);
+  }
+}
+
+//to search the superhero through API call
+async function searchInput(text) {
+  if (text.length != 0) {
+    let url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${text}&apikey=4a0de140540f73b7ebc8b9816b51defd&hash=ef2adfa67e8de20cc93d4d4c9be0b04f&ts=1`;
+    let response = await fetch(url);
+    const data1 = await response.json();
+    listOfHeros = data1.data.results;
+    if (listOfHeros.length === 0) {
+      renderHeroList();
+    } else {
+      renderHeroList();
+    }
+  }
+}
+
+//to add the fav superhero
+function addToFav(heroId, heroTitle) {
+  for (i of favList) {
+    if (i === heroId) {
+      errorMessage.innerHTML = "This hero is already in the Fav List";
+      //clearing out the message in 4 seconds
+      setTimeout(() => {
+        errorMessage.innerHTML = "";
+      }, 4000);
+
+      return;
+    }
+  }
+
+  //pushing fav superhero to the fav List array
+  favList.push(heroId);
+  errorMessage.innerHTML = `${heroTitle} added in the fav List`;
+  //clearing out the message in 4 seconds
+  setTimeout(() => {
+    errorMessage.innerHTML = "";
+  }, 4000);
+}
+
+function handleInput(input) {
+  let text = input.value;
+
+  searchInput(text);
+}
+
+//handling key and click events
+function handleKeyAndClick(e) {
+  if (e.target.id === "inputBar") {
+    handleInput(e.target);
+  }
+  if (e.target.id === "details") {
+    let heroId = e.target.dataset.id;
+    localStorage.setItem("heroId", JSON.stringify(heroId));
+
+    window.open("./details/details.html");
+  }
+  if (e.target.id === "favBtn") {
+    addToFav(e.target.dataset.id, e.target.dataset.title);
+  }
+  if (e.target.id === "favourite") {
+    localStorage.setItem("favHeros", JSON.stringify(favList));
+    window.open("./FavList/fav.html");
+  }
+  if (e.target.id === "clear") {
+    window.location.reload();
+  }
+}
+
+//event listeners
+document.addEventListener("keyup", handleKeyAndClick);
+document.addEventListener("click", handleKeyAndClick);
